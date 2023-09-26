069e61e35cb08b4d4c68f087ca74f43e619c1051
Pedro Nogueira <pedro.nogueira.barboza@gmail.com>
2023-09-26 09:48:45 -0300

    adicionei o  projeto

---
+++ etl.py
@@ -0,0 +1,78 @@
+import openai
+import csv
+import time
+
+sdw2023_api_url = 'https://sdw-2023-prd.up.railway.app'
+
+# instalei o pip, pandas e openai
+
+
+def ler_arquivo(nome="SDW2023.csv"):
+    arquivo_csv = open(nome, mode='r')
+    leitor_csv = csv.DictReader(arquivo_csv)
+
+# Lista para armazenar os dicionários dos usuários
+    lista_de_usuarios = []
+
+# Itere pelas linhas do arquivo CSV
+    for linha in leitor_csv:
+        usuario = {}
+        usuario['nome'] = linha['nome']
+        usuario['Perfil'] = linha['Perfil']
+        usuario['decisao'] = linha['decisao']
+        
+    # Adicione o dicionário do usuário à lista
+        lista_de_usuarios.append(usuario)
+    
+    return lista_de_usuarios
+        
+ 
+
+def gerar_mensagem(user):
+
+    openai.api_key = "************************************"
+    
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+        {
+            "role": "system",
+            "content": f"Você é um especialista em markting bancário e fará uma mensagem direcionada a um cliente que será solicitada."
+        },
+        
+        {
+            "role": "system",
+            "content": f"Para fazer a mensagem saiba que você terá que classificar o usuário em uma das seguintes categorias de tipo de investidor: conservador, arrojado ou moderado. Você terá as seguintes informações: o perfil do usuário que pode ser : Iniciante no assunto de finanças, Tradcional ou Antenado. E também é necesário que saiba que em uma situação de queda de 10% em um dos investimento do usário, ele Manter o valor investido, Vender o valor investido ou Aumentar o valor investido, isso pode ajudar a você elaborar melhor a mensagem e classíficá-lo em uma das categorias de tipo de investidor."
+        },
+                
+        {
+            "role": "user",
+            "content": f"Classifique o usuário {user['nome']} em uma das categorias de tipo de investidor: conservador, arrojado ou moderado, sabendo que : Ele tem perfil {user['Perfil']} e em uma situação de queda de 10% em um dos investimentos ele {user['decisao']} o valor investido. Como resposta, não diga as informações recebidas: perfil e situação de queda, diga ao usuário somente em que categoria se enquadra depois dê dicas de investimento a renda fixa, renda variável e fundos de investimento. Não pule linhas e faça isso em no máximo 400 caracteres."
+        }
+        ]
+    )
+    return completion.choices[0].message.content.strip('\"')
+
+
+
+
+if __name__ == "__main__":
+
+    users = ler_arquivo("SDW2023.csv")
+
+
+    for indice, user in enumerate(users):
+        print()
+        print(f"Nome do usuário: {user['nome']}\nPerfil: {user['Perfil']}\nDecisão: {user['decisao']} o valor investido em uma situação de queda de 10% em um dos investimentos.")
+        print()
+
+        mensagem = gerar_mensagem(user)
+        arquivo_csv = open("Banco_de_respostas.csv", mode='a')
+        arquivo_csv.write(f"Resposta do usuário {user['nome']}:\n {mensagem}\n\n")
+        print(mensagem)
+        time.sleep(21) # ("Esperando 21 segundos para não exceder o limite de requisições da API")
+
+
+    arquivo_csv.close()
+
+
